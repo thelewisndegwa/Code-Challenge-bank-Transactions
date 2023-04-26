7209af50b4f6bded267885284eba50a4c7880a4e
thelewisndegwa <lewiskanyi77@gmail.com>
2023-04-26 12:08:31 +0300

    code challene completed

--- bank-transactions/src/App.js
+++ bank-transactions/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import TransTable from './components/Table';
 import Form from './components/Form';
-import Filter from './components/Filter';
+
 function App() {
   return (
    <>
-   <Filter/>
+   
    <TransTable/>
     <Form/>
 
