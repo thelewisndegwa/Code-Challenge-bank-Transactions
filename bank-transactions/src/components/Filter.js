356ee20eba19f8352c6811887fef3cd824d6916b
thelewisndegwa <lewiskanyi77@gmail.com>
2023-04-26 11:58:51 +0300

    filter works

--- bank-transactions/src/components/Filter.js
+++ bank-transactions/src/components/Filter.js
@@ -1,27 +1,39 @@
 import React from "react"
 import { useState, useEffect } from "react"
 
+function Filter() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  useEffect(() => {
+    fetch('http://localhost:3003/transactions')
+      .then(response => response.json())
+      .then(data => setTransactions(data))
+      .catch(error => console.log(error))
+  }, []);
 
+  const handleSearch = e => {
+    setSearchTerm(e.target.value);
+  };
 
-function Filter(){
-    const [Transaction, setTransaction] = useState([]);
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    const [searchTerm, setSearchTerm] = useState('');
-   
-    const handleSearch = e => {
-        setSearchTerm(e.target.value);
-      };
-      const filteredTransaction = Transaction.filter(Transaction => Transaction.description.toLowerCase().includes(searchTerm.toLowerCase()));
- 
-
-    return (
-       <>
+  return (
+    <div>
       <form>
-        <input type="text" placeholder="Search by description" value={searchTerm} onChange={handleSearch} />
+        <input
+          type="text"
+          placeholder="Search by description"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
       </form>
+      
+      
+    </div>
+  )
+}
 
-</>
-    )
-        }
-  export default Filter;
+export default Filter;
